5aac5cd8abb4f1960282ce25ef0954a21b1d8df5
hector_gen <hector@nuevegen.net>
2023-04-25 14:08:33 +0200

    Learning about Do-while

--- base.js
+++ base.js
@@ -9,6 +9,18 @@ function getDate(){
     return f;
 }
 
+function createListElements(total){
+    let i = 0;
+    let ul = document.createElement("UL");
+    do{
+        let li = document.createElement("LI");
+        li.textContent = "A"+i;
+        ul.appendChild(li);
+        i++;
+    }while(i<total);
+    document.body.appendChild(ul);
+}
+
 window.onload = function(){
     // Mostramos la fecha actual
     let tagElement = document.getElementById("fecha");
@@ -31,4 +43,37 @@ window.onload = function(){
     else{
         saludoElement.innerText = "Buenas noches";
     }
+
+    // Mostramos el día de la semana
+    let diasemanaElement = document.getElementById("dia_semana");
+    let dia = "domingo";
+    switch (f.getDay()){
+        case 1:
+            dia = "lunes";
+            break;
+        case 2:
+            dia = "martes";
+            break;
+        case 3:
+            dia = "miércoles";
+            break;
+        case 4:
+            dia = "jueves";
+            break;
+        case 5:
+            dia = "viernes";
+            break;
+        case 6:
+            dia = "sábado";
+            break;
+    }
+    diasemanaElement.textContent = dia;
+
+    // Mostramos el día de la semana en el idioma español de ESPAÑA
+    // ref: https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/Date/toLocaleDateString
+    let options = { weekday: 'long'};
+    console.log(f.toLocaleDateString("es-ES", options));
+
+    //Creamos una lista dinámica con un DO-WHILE de 10 elementos
+    createListElements(10);
 }
